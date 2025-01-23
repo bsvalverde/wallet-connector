fedc2d6b29676bd45f1a3c429d64caf3761f168a
bsvalverde <bsvalverde@gmail.com>
2025-01-22 23:49:07 -0300

    feat: Shows wallet list

--- src/containers/AppContainer.tsx
+++ src/containers/AppContainer.tsx
@@ -1,4 +1,9 @@
+import WalletConnector from "../Connector/WalletConnector";
+
 export default function AppContainer() {
+  if (true) {
+    return <WalletConnector />;
+  }
   return (
     <p>
       has logic to connect. if not connected,show connect button. if connected,
